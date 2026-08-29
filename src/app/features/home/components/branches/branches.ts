0dc80dda0f8b38e6be915461977445f50c6d19ce
patrwan <p.gonzalezc4@outlook.es>
2026-08-28 22:35:10 -0400

    Add: branch section

---
+++ src/app/features/home/components/branches/branches.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-branches',
+  imports: [],
+  templateUrl: './branches.html',
+  styleUrl: './branches.css',
+})
+export class Branches {
+
+}
--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Hero } from '../../components/hero/hero';
 import { QuickSearch } from '../../components/quick-search/quick-search';
 import { ValueProposition } from '../../components/value-proposition/value-proposition';
+import { Branches } from '../../components/branches/branches';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, QuickSearch, ValueProposition],
+  imports: [Hero, QuickSearch, ValueProposition, Branches],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
